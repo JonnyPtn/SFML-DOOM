2b33fb70adc0a2f14ec5e32a93757104559ea614
Jonny Paton <jonathan.r.paton@googlemail.com>
2016-05-06 11:11:19 +0100

    Doofus

--- s_sound.cpp
+++ s_sound.cpp
@@ -356,7 +356,7 @@ S_StartSoundAtVolume
 		int i = 0;
 		while (i < dataSize)
 		{
-			newData.push_back(static_cast<sf::Int16>(data[i])<<7));
+			newData.push_back(static_cast<sf::Int16>(data[i])<<7);
 			i++;
 		}
 
