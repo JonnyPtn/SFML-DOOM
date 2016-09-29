eed87e8d0bbb9f6d209974d13a147f6f72812e53
Jonny Paton <jonathan.r.paton@googlemail.com>
2016-09-29 07:33:52 +0100

    Items shouldn't respawn when not in deathmatch

--- p_mobj.cpp
+++ p_mobj.cpp
@@ -563,8 +563,8 @@ void P_RespawnSpecials (void)
     int			i;
 
     // only respawn items in deathmatch
-/*    if (deathmatch != 2)	why would a bool ever equal 2?
-	return;	// */
+	if (deathmatch)
+		return;
 
     // nothing left to respawn?
     if (iquehead == iquetail)
