d6f050815929ca3cedb1872e67f1b313beb6b582
Jonny Paton <jonathan.r.paton@googlemail.com>
2025-09-17 15:00:45 +0100

    z_zone to cpp

--- linuxdoom-1.10/z_zone.cxx
+++ linuxdoom-1.10/z_zone.cxx
@@ -73,7 +73,7 @@ void Z_ClearZone (memzone_t* zone)
 	zone->blocklist.prev =
 	block = (memblock_t *)( (byte *)zone + sizeof(memzone_t) );
     
-    zone->blocklist.user = (void *)zone;
+    zone->blocklist.user = (void **)zone;
     zone->blocklist.tag = PU_STATIC;
     zone->rover = block;
 	
@@ -103,7 +103,7 @@ void Z_Init (void)
 	mainzone->blocklist.prev =
 	block = (memblock_t *)( (byte *)mainzone + sizeof(memzone_t) );
 
-    mainzone->blocklist.user = (void *)mainzone;
+    mainzone->blocklist.user = (void **)mainzone;
     mainzone->blocklist.tag = PU_STATIC;
     mainzone->rover = block;
 	
@@ -267,7 +267,7 @@ Z_Malloc
     if (user)
     {
 	// mark as an in use block
-	base->user = user;			
+	base->user = (void**)user;			
 	*(void **)user = (void *) ((byte *)base + sizeof(memblock_t));
     }
     else
@@ -276,7 +276,7 @@ Z_Malloc
 	    I_Error ("Z_Malloc: an owner is required for purgable blocks");
 
 	// mark as in use, but unowned	
-	base->user = (void *)2;		
+	base->user = (void **)2;		
     }
     base->tag = tag;
 
