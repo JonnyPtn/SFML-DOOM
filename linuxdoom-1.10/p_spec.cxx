34029b1d2741dbae41161b19956e975b16fcf2cd
Jonny Paton <jonathan.r.paton@googlemail.com>
2025-09-18 15:34:37 +0100

    Fix infinite loop in anim loading

--- linuxdoom-1.10/p_spec.cxx
+++ linuxdoom-1.10/p_spec.cxx
@@ -52,6 +52,7 @@ rcsid[] = "$Id: p_spec.c,v 1.6 1997/02/03 22:45:12 b1 Exp $";
 // Data.
 #include "sounds.h"
 
+#include <array>
 
 //
 // Animating textures and planes
@@ -98,38 +99,37 @@ extern anim_t*	lastanim;
 //  and end entry, in the order found in
 //  the WAD file.
 //
-animdef_t		animdefs[] =
+std::array<animdef_t, 22 >		animdefs = {
+
 {
-    {false,	"NUKAGE3",	"NUKAGE1",	8},
-    {false,	"FWATER4",	"FWATER1",	8},
-    {false,	"SWATER4",	"SWATER1", 	8},
-    {false,	"LAVA4",	"LAVA1",	8},
-    {false,	"BLOOD3",	"BLOOD1",	8},
+	{false,	"NUKAGE3",	"NUKAGE1",	8},
+	{false,	"FWATER4",	"FWATER1",	8},
+	{false,	"SWATER4",	"SWATER1", 	8},
+	{false,	"LAVA4",	"LAVA1",	8},
+	{false,	"BLOOD3",	"BLOOD1",	8},
 
-    // DOOM II flat animations.
-    {false,	"RROCK08",	"RROCK05",	8},		
-    {false,	"SLIME04",	"SLIME01",	8},
-    {false,	"SLIME08",	"SLIME05",	8},
-    {false,	"SLIME12",	"SLIME09",	8},
+	// DOOM II flat animations.
+	{false,	"RROCK08",	"RROCK05",	8},
+	{false,	"SLIME04",	"SLIME01",	8},
+	{false,	"SLIME08",	"SLIME05",	8},
+	{false,	"SLIME12",	"SLIME09",	8},
 
-    {true,	"BLODGR4",	"BLODGR1",	8},
-    {true,	"SLADRIP3",	"SLADRIP1",	8},
+	{true,	"BLODGR4",	"BLODGR1",	8},
+	{true,	"SLADRIP3",	"SLADRIP1",	8},
 
-    {true,	"BLODRIP4",	"BLODRIP1",	8},
-    {true,	"FIREWALL",	"FIREWALA",	8},
-    {true,	"GSTFONT3",	"GSTFONT1",	8},
-    {true,	"FIRELAVA",	"FIRELAV3",	8},
-    {true,	"FIREMAG3",	"FIREMAG1",	8},
-    {true,	"FIREBLU2",	"FIREBLU1",	8},
-    {true,	"ROCKRED3",	"ROCKRED1",	8},
+	{true,	"BLODRIP4",	"BLODRIP1",	8},
+	{true,	"FIREWALL",	"FIREWALA",	8},
+	{true,	"GSTFONT3",	"GSTFONT1",	8},
+	{true,	"FIRELAVA",	"FIRELAV3",	8},
+	{true,	"FIREMAG3",	"FIREMAG1",	8},
+	{true,	"FIREBLU2",	"FIREBLU1",	8},
+	{true,	"ROCKRED3",	"ROCKRED1",	8},
 
-    {true,	"BFALL4",	"BFALL1",	8},
-    {true,	"SFALL4",	"SFALL1",	8},
-    {true,	"WFALL4",	"WFALL1",	8},
-    {true,	"DBRAIN4",	"DBRAIN1",	8},
-	
-    {-1}
-};
+	{true,	"BFALL4",	"BFALL1",	8},
+	{true,	"SFALL4",	"SFALL1",	8},
+	{true,	"WFALL4",	"WFALL1",	8},
+	{true,	"DBRAIN4",	"DBRAIN1",	8},
+} };
 
 anim_t		anims[MAXANIMS];
 anim_t*		lastanim;
@@ -152,7 +152,7 @@ void P_InitPicAnims (void)
     
     //	Init animation
     lastanim = anims;
-    for (i=0 ; animdefs[i].istexture != -1 ; i++)
+    for (i=0 ; i < animdefs.size() ; i++)
     {
 	if (animdefs[i].istexture)
 	{
